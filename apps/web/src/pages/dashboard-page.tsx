ad5511b8f4a450639e4c7fdc03dc4cb6f3cec307
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2025-01-29 19:17:56 +0300

    feat: added tailwind

--- apps/web/src/pages/dashboard-page.tsx
+++ apps/web/src/pages/dashboard-page.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
 
 export const DashboardPage: FC = () => {
-  return <div>Dashboard page</div>;
+  return <div className="text-3xl font-bold underline">Dashboard page</div>;
 };
