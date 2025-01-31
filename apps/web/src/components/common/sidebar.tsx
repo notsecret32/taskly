097fa3f0125c8b3e03f0ef0c20b33aa5c16f0419
Alexander Skvorchevskiy <alex.skvorchevsky@gmail.com>
2025-01-31 21:31:03 +0300

    feat: increased the number of fake projects

--- apps/web/src/components/common/sidebar.tsx
+++ apps/web/src/components/common/sidebar.tsx
@@ -16,6 +16,14 @@ const projects: Project[] = [
     id: '3',
     title: 'Project #3',
   },
+  {
+    id: '4',
+    title: 'Project #4',
+  },
+  {
+    id: '5',
+    title: 'Project #5',
+  },
 ];
 
 export const Sidebar: FC = () => {
