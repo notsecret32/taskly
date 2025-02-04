5a2e924b2db727c2ab88197f83e057a5bb4181a0
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2025-02-04 19:14:58 +0300

    feat: added `react-query`

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,15 @@
+import { queryClient } from '@/config/react-query';
 import { router } from '@/pages';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { FC } from 'react';
 import { RouterProvider } from 'react-router';
 
 export const App: FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
